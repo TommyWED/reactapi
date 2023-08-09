5ee520e8b22d566ddfb067917b19b73bfc6e4acc
TommyWED <webdes4567@gmail.com>
2023-08-09 14:13:09 +0800

    install react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import SelectMOK from "./SelectMOK"
+
 
 function App() {
   return (
     <div className="App">
       <p>1234</p>
+      <SelectMOK />
     </div>
   );
 }
---
+++ src/SelectMOK.jsx
@@ -0,0 +1,13 @@
+
+
+export default function SelectMOK (){
+
+    return (
+        <div>
+            <input type="radio" value="MTR" id="MTR" name="options"></input>
+            <label for="MTR">MTR</label>
+            <input type="radio" value="KMB" name="options"></input>
+            <label for="KMB">KMB</label>
+        </div>
+    )
+}
