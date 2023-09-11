7dc53dd5bfe80b98e2e3f75a351b61c6f234ff78
TommyWED <webdes4567@gmail.com>
2023-09-12 00:03:18 +0800

    update TKL dest

--- src/Mtrtime.jsx
+++ src/Mtrtime.jsx
@@ -8,7 +8,8 @@ export default function Mtrtime( {line, station,lang}){
     const LS = `${line}-${station}`
     const destobj =({"HOK":["Hong Kong", "香港"], "AWE": ["AsiaWorld Expo", '博覽會'],"TUC": ["Tung Chung", '東涌'],"WKS": ['Wu Kai Sha', '烏溪沙'] ,
     'TUM': ['Tuen Mun', '屯門'], 'NOP':  ['North Point', '北角'], 'POA': ['Po Lam', '寶琳'], 'LHP': ['LOHAS Park', '康城'], 'ADM':['Admiralty', '金鐘'], 'LOW': [ 'Lo Wu', '羅湖'], 'LMC': ['Lok Ma Chau', '落馬洲'], 
-    'SOH': ['South Horizons', '海怡半島'], 'CEN': ['Central', '中環'], 'TSW': ['Tsuen Wan', '荃灣'], 'CHW': ['Chai Wan', '柴灣'], 'KET': ['Kennedy Town', '堅尼地城'],'SHS': ['Sheung Shui', '上水']})
+    'SOH': ['South Horizons', '海怡半島'], 'CEN': ['Central', '中環'], 'TSW': ['Tsuen Wan', '荃灣'], 'CHW': ['Chai Wan', '柴灣'], 'KET': ['Kennedy Town', '堅尼地城'],'SHS': ['Sheung Shui', '上水'],'HUH': ['Hung Hom', '紅磡']
+    ,'TAW': ['Tai Wai', '大圍'], 'TIK': [ 'Tiu Keng Leng', '調景嶺'] })
     const [TOFUP, setTOFUP] = useState(false)
     const [TOFD, setTOFD] = useState(false)
     const direction = ["destination","目的地"]
